5aade08f553a39aee8e7bca549b3be955aeffba7
Iain Taylor <iain.taylor.94@gmail.com>
2023-09-04 14:48:23 +0000

    copied program

--- .ccls-cache/@home@runner@Ch11Q1P8-using-pointers-and-functions/main.c
+++ .ccls-cache/@home@runner@Ch11Q1P8-using-pointers-and-functions/main.c
@@ -1,6 +1,21 @@
+// Program to illustrate using pointers and functions
+
 #include <stdio.h>
 
+// Declare Functions
+void test (int *int_pointer) {
+  *int_pointer = 100;
+}
+
 int main(void) {
-  printf("Hello World\n");
+
+  int i = 50, *p = &i;
+
+  printf ("Before the call to test i = %d\n", i);
+
+  test (p);
+
+  printf ("After the call to test i = %d\n", i);
+  
   return 0;
 }
